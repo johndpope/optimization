56998d9b86202a87f8fc1e1d55547af8f8445607
John Pope <jp@fieldstormapp.com>
2018-06-15 10:14:01 -0400

    updates

--- cythonize.sh
+++ cythonize.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd build
 cython -a ../RasterSpace.pyx -o RasterSpace.c
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing \
-      -I/usr/include/python2.7 -o RasterSpace.so RasterSpace.c
+gcc -stdlib=libc++ -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing \
+      -I/System/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 -o RasterSpace.so RasterSpace.c -v
 cp RasterSpace.so ../
